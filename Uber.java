05d32cf1dbac67ad3e6a1d2f461f5a447c70d1cf
PramodKumar922 <pramod.tyn@gmail.com>
2024-11-02 15:27:35 +0530

    Uber.java

---
+++ Uber.java
@@ -0,0 +1,5 @@
+Uber Technologies, Inc. is an American multinational transportation company that provides ride-hailing services, courier services, food delivery, and freight transport.
+It is headquartered in San Francisco, California, and operates in approximately 70 countries and 10,500 cities worldwide.
+It is the largest ridesharing company worldwide with over 150 million monthly active users and 6 million active drivers and couriers.
+It facilitates an average of 28 million trips per day and has facilitated 47 billion trips since its inception in 2010.
+In 2023, the company had a take rate (revenue as a percentage of gross bookings) of 28.7% for mobility services and 18.3% for food delivery.
